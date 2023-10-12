db1ae08f0a97cb570a75eb690da5be6e0b791d04
BimalKanxa <sharmabimal2003@gmail.com>
2023-10-12 17:00:51 +0530

    added cards

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import '../css/Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <section className="subfooter">
+    <div className="container3 flex3 subfooter-container">
+      <h3>The Smart City Parking</h3>
+      <a href="#" className="hover-links"><i className="fa-brands fa-facebook"></i></a>
+      <a href="#" className="hover-links"><i className="fa-brands fa-instagram"></i></a>
+      <a href="#" className="hover-links"><i className="fa-brands fa-linkedin"></i></a>
+      <a href="#" className="hover-links"><i className="fa-brands fa-x-twitter"></i></a>
+<span>copyright &copy; 2023</span> 
+    </div>
+  </section>
+    </div>
+  )
+}
+
+export default Footer
